08a6042e8a9ccf81d4fc0cc09126c9e0dada684f
mahadi-nsu <mahadi.hassan@northsouth.edu>
2019-05-28 09:39:57 +0600

    weather data fetched from open weather map api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const API_KEY = "48b0c5fe39d7f2112578c50444316b97";
+
+
 class App extends Component {
   state = {
-    city: "dhaka"
+    city: "dhaka",
+    temp: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
   };
 
   handleChange = event => {
     this.setState({ city: event.target.value });
   };
 
+  getWeather = async e => {
+    try {
+      const api_call = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${
+          this.state.city
+        },Bangladesh&appid=${API_KEY}&units=metric`
+      );
+      const data = await api_call.json();
+      this.setState({
+        temp: data.main.temp,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } catch (err) {
+      this.setState({
+        error: err
+      });
+    }
+
+    // console.log(data);
+  };
+
+
   handleSubmit = event => {
     event.preventDefault();
     alert(this.state.city);
+    this.getWeather();
   };
 
   render() {
